83a76ea3acb473f7b87f615cd6f0a0db311a98bf
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-11-02 11:20:07 +0400

    commited

---
+++ lesson1/task1/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/index.jsx",
+    output: {
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.(js|jsx?)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new MomentLocalesPlugin(),
+      new MomentLocalesPlugin({
+        localesToKeep: ["es-us", "ru"],
+      }),
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      open: true,
+      hot: true,
+      port: 8080,
+      historyApiFallback: true,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
+};
