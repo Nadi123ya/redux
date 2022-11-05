8a9685f103dfe5ce2a7fabb5d01dd0566bfc1e60
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-11-05 12:06:36 +0400

    comited

--- lesson2/task2/src/store.js
+++ lesson2/task2/src/store.js
@@ -35,7 +35,7 @@ const counterReducer = (state = initialState, action) => {
       return {
         ...state,
         // value: state.value - 1,
-        history: state.history.concat(-1),
+        history: state.history.concat("-1"),
       };
       case RESET:
       return {
