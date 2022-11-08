e3d6c316d4904760512403fd786a67701acf08b7
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-11-08 15:14:29 +0400

    commited

--- lesson5/task2/src/options/options.selectors.js
+++ lesson5/task2/src/options/options.selectors.js
@@ -1,15 +1,34 @@
+import { createSelector } from "reselect";
+
 export const optionsListSelector = (state) => {
   return state.options.optionsList;
 };
+export const selectedIdSelector = (state) => {
+  return state.options.selected;
+};
 
-export const selectedOptionsSelector = state => {
- const allOprtions = state.options.optionsList;
- const selectedIds = state.options.selected;
- return allOprtions.filter(option => selectedIds.includes(option.id))
-}
+// export const selectedOptionsSelector = state => {
+//  const allOptions = optionsListSelector(state);
+//  const selectedIds = selectedIdSelector(state);
+//  return allOptions.filter(option => selectedIds.includes(option.id))
+// }
 
-export const availableOptionsSelector = state => {
-  const allOprtions = state.options.optionsList;
-  const selectedIds = state.options.selected;
-  return allOprtions.filter(option => !selectedIds.includes(option.id))
- }
+export const selectedOptionsSelector = createSelector(
+  [optionsListSelector, selectedIdSelector],
+  (allOptions, selectedIds) => {
+    return allOptions.filter((option) => selectedIds.includes(option.id));
+  }
+);
+
+// export const availableOptionsSelector = (state) => {
+//   const allOprtions = optionsListSelector(state);
+//   const selectedIds = selectedIdSelector(state);
+//   return allOprtions.filter((option) => !selectedIds.includes(option.id));
+// };
+
+export const availableOptionsSelector = createSelector(
+  [optionsListSelector, selectedIdSelector],
+  (allOptions, selectedIds) => {
+    return allOptions.filter((option) => !selectedIds.includes(option.id));
+  }
+);
